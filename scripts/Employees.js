fe16a52069b4f1564c086d284246c36b37f551da
minidiscs <shanedbutler@gmail.com>
2022-07-05 11:20:14 -0400

    add click listener and alert for num products sold

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,12 +1,13 @@
-import { getEmployees } from "./database.js"
+import { getEmployees, getOrders } from "./database.js"
 
 const employees = getEmployees()
+const orders = getOrders()
 
 export const Employees = () => {
     let html = "<ul>"
 
     for (const employee of employees) {
-        html += `<li id="employee--${employee.Id}">${employee.name}</li>`
+        html += `<li id="employee--${employee.id}">${employee.name}</li>`
     }
 
     html += "</ul>"
@@ -14,3 +15,28 @@ export const Employees = () => {
     return html
 }
 
+const productsSold = (employee) => {
+    let productsSoldNum = 0
+    for (const order of orders) {
+        if (employee.id === order.employeeId) {
+            productsSoldNum++
+        }
+    }
+    return productsSoldNum
+}
+
+document.addEventListener("click", (clickEvent) => employeeSalesAlert(clickEvent))
+
+const employeeSalesAlert = (clickEvent) => {
+    const itemClicked = clickEvent.target
+    if (itemClicked.id.startsWith("employee")) {
+        const [,employeeId] = itemClicked.id.split("--")
+
+        for (const employee of employees) {
+            if (employee.id === parseInt(employeeId)) {
+                const productsSoldNum = productsSold (employee)
+                window.alert(`${employee.name} has sold ${productsSoldNum} products`)
+            }
+        }
+    }
+}
